2c27bec6e1a141b6947dc1b64b35be40f109eaea
Flávio Félix <fau_felix30@hotmail.com>
2024-09-06 16:17:28 -0300

    feat: acrescentando uma features pesquisar no projeto

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log(dados);
+function pesquisar() {
+  // Seleciona a seção onde os resultados serão exibidos
+  let section = document.getElementById('resultados-pesquisa');
+
+  // Inicializa uma string vazia para armazenar o HTML dos resultados
+  let resultados = "";
+
+  // Itera sobre cada resultado da pesquisa
+  for (let dado of dados) {
+    // Cria o HTML para um item de resultado
+    resultados += `
+      <div class="item-resultado">
+        <h2>
+          <a href="#" target="_blank">${dado.title}</a> </h2>
+        <p class="descricao-meta">${dado.descricao}</p> <a href="${dado.link}" target="_blank">Mais informações</a> </div>
+    `;
+  }
+
+  // Insere o HTML gerado na seção
+  section.innerHTML = resultados;
+}
