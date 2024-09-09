85ddcc6b5330bafca9140396c02878682d6624f8
Flávio Félix <fau_felix30@hotmail.com>
2024-09-09 16:56:53 -0300

    Finalizado o projeto

--- app.js
+++ app.js
@@ -2,20 +2,45 @@ function pesquisar() {
   // Seleciona a seção onde os resultados serão exibidos
   let section = document.getElementById('resultados-pesquisa');
 
-  // Inicializa uma string vazia para armazenar o HTML dos resultados
-  let resultados = "";
+  let campoPesquisa = document.getElementById('campo-pesquisa').value;
 
-  // Itera sobre cada resultado da pesquisa
-  for (let dado of dados) {
-    // Cria o HTML para um item de resultado
-    resultados += `
-      <div class="item-resultado">
-        <h2>
-          <a href="#" target="_blank">${dado.title}</a> </h2>
-        <p class="descricao-meta">${dado.descricao}</p> <a href="${dado.link}" target="_blank">Mais informações</a> </div>
-    `;
+  // se o campoPesquisa for uma string vazia
+  if(!campoPesquisa){
+    section.innerHTML = "<p>Nenhum atleta encontrado!Você precisa digitar nome de um atleta ou esporte!</p>"
+    return;
   }
+  //campoPesquisa = campoPesquisa.toLoweCase();
 
-  // Insere o HTML gerado na seção
-  section.innerHTML = resultados;
+  // Verifica se dados está definido e não está vazio
+  if (dados && dados.length > 0) {
+    // Inicializa uma string vazia para armazenar o HTML dos resultados
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada resultado da pesquisa
+    for (let dado of dados) {
+      titulo = dado.titulo.toLowerCase();
+      descricao = dado.descricao.toLowerCase();
+      tags = dado.tags.toLowerCase();
+
+      // Cria um novo elemento
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        resultados += `
+          <div class="item-resultado">
+            <h2>
+              <a href="#" target="_blank">${dado.titulo}</a> </h2>
+            <p class="descricao-meta">${dado.descricao}</p> <a href="${dado.link}" target="_blank">Mais informações</a> </div>
+        `;
+      }
+    }
+
+    if(!resultados) {
+      resultados = "<p>Nenhum atleta encontrado!</p>";
+    }
+
+    // Insere o HTML gerado na seção
+    section.innerHTML = resultados;
+  } 
 }
--- dados.js
+++ dados.js
@@ -1,18 +1,21 @@
 let dados = [ 
   {
-    title: "Rayssa Leal",
+    titulo: "Rayssa Leal",
     descricao: "Jhulia Rayssa Mendes Leal, conhecida como 'Fadinha', é uma skatista brasileira nascida em Imperatriz, Maranhão, em 4 de janeiro de 2008. Ela se destacou ao conquistar a medalha de prata nos Jogos Olímpicos de Tóquio 2020, tornando-se a mais jovem medalhista olímpica brasileira. Em 2024, Rayssa conquistou o bronze nos Jogos Olímpicos de Paris, além de ser campeã pan-americana e bicampeã mundial no skate street.",
-    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"
   },
   {
-    title: "Rebeca Andrade",
+    titulo: "Rebeca Andrade",
     descricao: "Rebeca Andrade é uma destacada ginasta brasileira, conhecida por sua trajetória de superação e conquistas notáveis no esporte. Nascida em Guarulhos, ela começou a treinar ginástica artística aos quatro anos, enfrentando desafios financeiros e lesões ao longo de sua carreira, incluindo três cirurgias no joelho. Rebeca ganhou notoriedade internacional ao conquistar duas medalhas nos Jogos Olímpicos de Tóquio em 2021, incluindo a primeira medalha de ouro da ginástica feminina brasileira.",
-    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginática ginastica ouro"
   },
   {
-    title: "Bia Souza",
+    titulo: "Beatriz Souza",
     descricao: "Beatriz Souza, conhecida como Bia Souza, é uma judoca brasileira nascida em 1998. Ela se destacou no judô ao conquistar medalhas em competições internacionais, incluindo o Campeonato Mundial de Judô e os Jogos Pan-Americanos. Bia é reconhecida por sua técnica e força, representando o Brasil nas categorias de peso leve e meio-médio, e se tornou uma referência no esporte.",
-    link: "https://pt.wikipedia.org/wiki/Bia_Souza"
+    link: "https://pt.wikipedia.org/wiki/Bia_Souza",
+    tags: "judô judo judoca judoka"
   }
 ];
 
